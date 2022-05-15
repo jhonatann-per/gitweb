1bafc8b31297e77dfc66f3948403f5e2389bdc96
jhonatan pereira <Jhonatan pereira>
2022-05-15 02:45:47 -0300

    input finished table in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,36 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: '14px',
+        md: '16px',
+        lg: '20px',
+        xl: '24px',
+        '2xl': '32px',
+        '3xl': '48px',
+        '4xl': '64px',
+        '5xl': '96px',
+        '6xl': '128px',
+      },
+      colors: {
+        white: {
+          DEFAULT: '#FAFAFA',
+        },
+        black: {
+          DEFAULT: '#3D3D3D',
+        },
+        blue: {
+          100: '#EDF3F7'
+        },
+        gray: {
+          DEFAULT: '#989AA0',
+        }
+      }
+    },
   },
   plugins: [],
 }
